e1b84854c7a7119352ed44b16d116cb79b2191eb
daoduytan <ddtan2212@gmail.com>
2018-03-18 12:37:54 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
-
+const stripe = require('stripe')('sk_test_MoKet6Hjhj8kbBm1idM8noGp');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
+// const keys = require('../config/keys');
+
+app.post('/api/stripe', async (req, res) => {
+  console.log(req.token);
+
+  const charge = await stripe.charges.create({
+    amount: 500,
+    currency: 'usd',
+    description: '$5 for 5 credits',
+    source: req.token
+  }, function() {
+    if(err) {
+      console.log(err);
+      res.send('Failed')
+     } else {
+       console.log('success payment', charge);
+       res.send(charge)
+     }
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
